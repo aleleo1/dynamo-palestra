6fbf6f7eb4bf470e34eae1b644bd6cd13db0f423
aleleo1 <100156426+aleleo1@users.noreply.github.com>
2024-04-30 12:28:20 +0200

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), auth()],
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
