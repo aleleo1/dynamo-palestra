53f880700b33ed16d6773a177655cfa14998e4be
aleleo1 <100156426+aleleo1@users.noreply.github.com>
2024-05-08 22:58:55 +0200

    test 18?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
-import zeabur from '@zeabur/astro-adapter/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), auth()],
   output: 'server',
-  adapter: zeabur()
+  adapter: vercel()
 });
